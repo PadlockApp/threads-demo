83a83ccb7eb3362ba585c091254adf557dfadb0b
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-07-10 01:23:05 +0300

    More implementation

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { KeyInfo, ThreadID } from '@textile/hub'
+import { KeyInfo, ThreadID, Identity } from '@textile/hub'
 import { Database } from "@textile/threads-database"
+import { Libp2pCryptoIdentity } from '@textile/threads-core'
 import { config } from 'dotenv'
 
 // Load your .env into process.env
@@ -10,6 +11,11 @@ const init = (keyInfo: KeyInfo, threadID: ThreadID) => {
     return db
 }
 
+const startThread = async (db: Database, threadID: ThreadID, identity: Identity) => {
+    await db.start(identity, { threadID: threadID })
+    return threadID
+}
+
 const example = async () => {
     const keyInfo: KeyInfo = {
         // Using insecure keys
@@ -18,10 +24,10 @@ const example = async () => {
         // @ts-ignore
         type: 1,
     }
-    const id = ThreadID.fromString('bafkr5u42tlm553spyrchiu6g44hzxi2l6bzmsl3wiibn5r7vlq3ccvq')
-
-    const db = await init(keyInfo, id)
-
+    const threadID = ThreadID.fromString('bafkr5u42tlm553spyrchiu6g44hzxi2l6bzmsl3wiibn5r7vlq3ccvq')
+    const db = await init(keyInfo, threadID)
+    const identity = await Libp2pCryptoIdentity.fromRandom()
+    const thread = await startThread(db, threadID, identity)
 }
 
 
