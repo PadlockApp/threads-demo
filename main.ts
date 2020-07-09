8d978016e00f62ffbbb5644aeea3b0fe4c322cee
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-07-10 02:09:29 +0300

    Enabled adding entries to collection

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-;(global as any).WebSocket = require('isomorphic-ws')
+; (global as any).WebSocket = require('isomorphic-ws')
 import { KeyInfo, ThreadID, Identity } from '@textile/hub'
-import { Database } from "@textile/threads-database"
+import { Database, Collection, JSONSchema } from "@textile/threads-database"
 import { Libp2pCryptoIdentity } from '@textile/threads-core'
 import { config } from 'dotenv'
 
@@ -17,6 +17,57 @@ const startThread = async (db: Database, threadID: ThreadID, identity: Identity)
     return threadID
 }
 
+interface Message {
+    _id: string
+    author: string
+    text: string
+}
+
+const messageSchema: JSONSchema = {
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    definitions: {
+        ChatBasic: {
+            title: 'ChatBasic',
+            type: 'object',
+            properties: {
+                _id: {
+                    type: 'string',
+                },
+                text: {
+                    type: 'string',
+                },
+                author: {
+                    type: 'string',
+                },
+            },
+            required: ['text', 'author', '_id'],
+        },
+    },
+}
+
+const newCollection = async (db: Database, roomName: string): Promise<Collection<Message>> => {
+    const chat = await db.newCollection<Message>(roomName, messageSchema)
+    return chat
+}
+
+const addListener = async (db: Database, name: string) => {
+    const filter = `${name}.*.0` // filter to our chat room collection
+    db.emitter.on(filter, (values: any, type: any) => {
+        //console.log(values)
+        const message: Message = values.event.patch;
+        console.log(message)
+    })
+}
+
+const send = async (collection: Collection<Message>, text: string, author: string) => {
+    const message: Message = {
+        _id: '',
+        author: author,
+        text: text,
+    }
+    await collection.insert(message)
+}
+
 const example = async () => {
     const keyInfo: KeyInfo = {
         // Using insecure keys
@@ -25,10 +76,19 @@ const example = async () => {
         // @ts-ignore
         type: 1,
     }
-    const threadID = ThreadID.fromString('bafkr5u42tlm553spyrchiu6g44hzxi2l6bzmsl3wiibn5r7vlq3ccvq')
+    const threadID = ThreadID.fromRandom()
     const db = await init(keyInfo, threadID)
     const identity = await Libp2pCryptoIdentity.fromRandom()
     const thread = await startThread(db, db.threadID, identity)
+
+    await addListener(db, 'room101')
+
+    const chat = await newCollection(db, 'room101')
+
+
+    setTimeout(() => {
+        send(chat, 'hello', 'Ahmed')
+    }, 2000)
 }
 
 example()
